de7901d8e8cb4b2656d5f530b1079e9a9bd4f12e
ASPeXP <prapaths@hotmail.com>
2023-10-19 08:36:56 +0700

    feature :sparkle: add display func

--- main.go
+++ main.go
@@ -9,8 +9,13 @@ func sum(a, b int) int {
 func div(a, b int) int {
 	return a / b
 }
+
+func display(msg string) {
+	fmt.Println(msg)
+}
 func main() {
 	fmt.Println("Hello, Github")
 	fmt.Println(sum(1, 2))
 	fmt.Println(div(1, 2))
+	display("Hello GitFlow from Me")
 }
