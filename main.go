1d5958991dd79546792fc7a43885bd2d8db172a8
ASPeXP <prapaths@hotmail.com>
2023-10-19 08:11:28 +0700

    feature :sparkle: add sub func

--- main.go
+++ main.go
@@ -8,8 +8,12 @@ func sum(a, b int) int {
 func multi(a, b int) int {
 	return a * b
 }
+func sub(a, b int) int {
+	return a - b
+}
 func main() {
 	fmt.Println("Hello, Github")
 	fmt.Println(sum(1, 2))
 	fmt.Println(multi(3, 2))
+	fmt.Println(sub(1, 2))
 }
