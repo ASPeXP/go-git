e02d5e91cffeb106919eb8e8bccef712a1212f4c
ASPeXP <prapaths@hotmail.com>
2023-10-19 07:59:18 +0700

    Feature :sparkle multi completed

--- main.go
+++ main.go
@@ -5,7 +5,11 @@ import "fmt"
 func sum(a, b int) int {
 	return a + b
 }
+func multi(a, b int) int {
+	return a * b
+}
 func main() {
 	fmt.Println("Hello, Github")
 	fmt.Println(sum(1, 2))
+	fmt.Println(multi(3, 2))
 }
